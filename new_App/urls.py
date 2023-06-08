00980501531186225d93a73d0c474fbbbd52434f
jaymiskin <miskinjay@gmail.com>
2023-06-08 18:20:51 +0530

    Add Templates and Codes

--- new_App/urls.py
+++ new_App/urls.py
@@ -5,7 +5,14 @@ from .views import *
 urlpatterns = [
     path('signin_page/', signin_page, name='signin_page'),
     path('index/', index, name='index'),
+    path('profile_page/', profile_page, name='profile_page'),
+
+
+
     path('', signup, name='signup'),
     path('signin/', signin, name='signin'),
+    path('profile_update/', profile_update, name='profile_update'),
+    path('profile_data/', profile_data, name='profile_data'),
     path('logout/', logout, name='logout'),
+    path('delete_account_function/',delete_account_function,name='delete_account_function'),
 ]
--- new_App/views.py
+++ new_App/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.conf import settings
+
+import os
 
 
+data={}
 def index(request):
     return render(request,'index.html')
 
 def signin_page(request):
     return render(request,'signin_page.html')
+
+def profile_page(request):
+    return render(request,'profile_page.html')
 
 
 #signup_functionality
@@ -39,7 +46,7 @@ def signin(request):
         master = Master.objects.get(Email = request.POST['email'])
         if master.Password == request.POST['password']:
             request.session['email'] = master.Email
-            return redirect(index)
+            return redirect(profile_page)
 
         else:
             return render(request, "signin_page.html",{'error':"Password does not match"})
@@ -47,6 +54,54 @@ def signin(request):
         return render(request,"signin_page.html",{'error':"User does not Exists Please SignUp"})
 
 
+
+# load profile data
+def profile_data(request):
+    master = Master.objects.get(Email = request.session['email'])
+    user_profile = Common.objects.get(Master = master)
+
+    user_profile.first_name = user_profile.Name.split()[0]
+    user_profile.last_name = user_profile.Name.split()[1]
+
+
+    user_profile.DateOfBirth = user_profile.DateOfBirth.strftime("%Y-%m-%d")
+    user_profile.DateOfJoining = user_profile.DateOfJoining.strftime("%Y-%m-%d")
+
+    data['user_data'] = user_profile
+    
+    return redirect(profile_page)
+
+main_path = settings.MEDIA_ROOT
+
+
+# profile update functionality student
+def profile_update(request):
+    print(request.POST)
+    master = Master.objects.get(Email = request.session['email'])
+    user_profile = Common.objects.get(Master = master)
+
+
+    user_profile.Name =' '.join([request.POST['first_name'], request.POST['last_name']])
+    user_profile.DateOfBirth = request.POST['dateofbirth']
+    user_profile.DateOfJoining = request.POST['dateofjoining']
+    user_profile.Address = request.POST['address']
+     
+    file_path = os.path.join(main_path, 'profiles')
+
+
+    user_profile.save()
+
+    return redirect(profile_page)
+
+
+# Delete account Functionality
+def delete_account_function(request):
+    print(request.POST)
+    master=Master.objects.get(Email = request.session['email'])
+    master.delete()
+    return redirect(signup) 
+
+# Logout Functionality
 def logout(request):
     if 'email' in request.session:
         del request.session['email']
